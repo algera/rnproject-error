f894b3c7d5e39b8a9bed635495bb25182fe45078
Jeff Algera <jeff@peerspace.com>
2020-07-28 16:24:57 -0700

    Initial dump

---
+++ ios/Bruh/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Bruh
+//
+//  Created by Jeff Algera on 7/28/20.
+//  Copyright © 2020 Jeff Algera. All rights reserved.
+//
+
+import UIKit
+import React
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
